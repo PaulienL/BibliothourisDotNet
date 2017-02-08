8cea6e29d2fd59b596ee5005169bd5a0fd4f360d
Wouter Bauweraerts <wouter.bauweraerts@cegeka.com>
2017-02-08 10:48:46 +0100

    created IPersonRepository

--- Bibliothouris/CGKSBibliothouris/Model/Repositories/IPersonRepository.cs
+++ Bibliothouris/CGKSBibliothouris/Model/Repositories/IPersonRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CGKSBibliothouris.Model.DomainModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,5 +7,9 @@ using System.Threading.Tasks;
 
 namespace CGKSBibliothouris.Model.Repositories {
     interface IPersonRepository {
+        Person CreatePerson(Person person);
+        Person ReadPerson(int id);
+
+        IEnumerable<Person> ReadAllPersons();
     }
 }
