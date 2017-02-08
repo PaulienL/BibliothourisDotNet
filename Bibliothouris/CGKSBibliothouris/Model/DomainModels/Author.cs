37bb265daa2f9576dab768b74f56b68e07f7d4f1
Paulien Lemay <paulien.lemay@cegeka.com>
2017-02-08 10:14:40 +0100

    start

---
+++ Bibliothouris/CGKSBibliothouris/Model/DomainModels/Author.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CGKSBibliothouris.Model.DomainModels
+{
+    class Author
+    {
+        public int Id { get; set; }
+        public string firstName { get; set; }
+        public string secondName { get; set; }
+
+        public Author(string firstName, string secondName)
+        {
+            this.firstName = firstName;
+            this.secondName = secondName; 
+        }
+    }
+}
