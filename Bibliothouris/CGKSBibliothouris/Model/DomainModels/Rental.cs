a12a02c9e17ffff761e2a2c2f4445ac6550d76a6
Paulien Lemay <paulien.lemay@cegeka.com>
2017-02-09 15:21:56 +0100

    rental

---
+++ Bibliothouris/CGKSBibliothouris/Model/DomainModels/Rental.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CGKSBibliothouris.Model.DomainModels
+{
+    class Rental
+    {
+        public Member member { get; set; }
+        public Book book { get; set; }
+        public DateTime duedate { get; set; }
+
+        
+    }
+}
